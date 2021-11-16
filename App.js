70fdcd4db2239deac8033586367d26f4ba251a94
Tayyaba sikander <tayyaba.sikander2000@gmail.com>
2021-11-16 12:03:29 +0500

    done with app.js

--- App.js
+++ App.js
@@ -2,20 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// an npm package that we have installed for navigations
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+// importing required components
+import LandingScreen from './components/auth/Landing'
+
+//it will contain our screens, pages and will be generating all routes.
+const Stack=createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Landing'>
+        {/* here options is the customization that we apply if want to each screen */}
+        <Stack.Screen name='Landing' component={LandingScreen} options={{headerShown:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
