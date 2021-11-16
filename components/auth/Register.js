5509af8bc49f8017c747d212d37615e7afbbb9d3
Tayyaba sikander <tayyaba.sikander2000@gmail.com>
2021-11-16 12:13:36 +0500

    created register.js and landing.js

---
+++ components/auth/Register.js
@@ -0,0 +1,45 @@
+// we created this component as class-component because it will contain state
+// funtions are statless while components are stateful
+import React, { Component } from 'react'
+import {Button, TextInput, View} from 'react-native'
+
+export class Register extends Component {
+
+    // to initialize the component. first function that will run
+    constructor(props){
+        super(props)
+
+        this.state={
+            email:'',
+            password: '',
+            name:''
+        }
+        // binding this in order to access it
+        this.onSignUp=this.onSignUp.bind(this)
+    }
+
+    onSignUp()
+    render() {
+        return (
+            <View>
+                <TextInput 
+                    placeholder='name' 
+                    onChangeText={(name)=>this.setState({name})}
+                />
+                <TextInput 
+                    placeholder='email' 
+                    onChangeText={(email)=>this.setState({email})}
+                />
+                <TextInput 
+                placeholder='password' 
+                secureTextEntry={true}
+                onChangeText={(password)=>this.setState({password})}
+            />
+            <Button title="Sign Up" onPress={()=>this.onSignUp()} />
+            </View>
+        )
+    }
+    
+}
+
+export default Register
